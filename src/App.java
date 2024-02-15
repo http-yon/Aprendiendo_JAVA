5fc62a71ea4d160dea06dabd745771a2d9ce8f84
http-yon <yohanpedraza2006@gmail.com>
2024-02-14 21:34:32 -0500

    feat: :tada: Aprendiendo JAVA !!!
    adentrandome a este mundo de java, en este caso y como de costumbre, haciendo un "hola mundo"

---
+++ src/App.java
@@ -0,0 +1,12 @@
+//clase App
+public class App {
+    //funcion main
+    public static void main(String[] args){
+        //impirmir en consola
+        System.out.print("Hello, World!");
+        System.out.println("Hello world");
+
+        /* comentario de varias lineas */
+
+    }
+}
